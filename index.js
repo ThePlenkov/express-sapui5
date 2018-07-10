157806ba1462ae2734793f5377827f4776a60ad6
Petr Plenkov <petr.plenkov@booking.com>
2018-07-10 11:06:51 +0200

    using CDN

--- index.js
+++ index.js
@@ -1,89 +1,85 @@
-'use strict';
+"use strict";
 
-const proxy = require('http-proxy-middleware');
-const url = require('url');
-const express = require('express');
+const proxy = require("http-proxy-middleware");
+const url = require("url");
+const express = require("express");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 
-module.exports = function (
-    oSettings
-) {
+module.exports = function(oSettings) {
+  var oNeoApp = oSettings.neoApp,
+    oDestinations = oSettings.destinations,
+    oManifest = oSettings.manifest,
+    oAgent = oSettings.agent;
 
-    var oNeoApp = oSettings.neoApp,
-        oDestinations = oSettings.destinations,
-        oManifest = oSettings.manifest,
-        oAgent = oSettings.agent;
+  var app = express();
 
-    var app = express();
+  let cdn = oSettings.cdn || "https://ui5.sap.com";
+  if (oSettings.version) {
+    cdn += "/" + oSettings.version;
+  }
 
-    if (oNeoApp && oNeoApp.routes) {
-        oNeoApp.routes.forEach(function (oRoute) {
+  const homePage =
+    "/test-resources/sap/ushell/shells/sandbox/fioriSandbox.html";
+  // redirect to FLP
+  app.get("/", async (req, res) => {
+    res.redirect(homePage);
+  });
 
-            var oTarget = oRoute.target;
-            if (oTarget) {
-
-                // proxy options
-                var oOptions = {};
-
-                // search for destination
-                if (oDestinations && oTarget.name) {
-                    var oDestination = oDestinations[oTarget.name];
-                    if (oDestination) {
-                        oOptions.target = oDestination.target;
-                        oOptions.changeOrigin = true;
-                        if (oDestination.useProxy) {
-                            oOptions.agent = oAgent;
-                        }
-                        if (oTarget.version) {
-                            oOptions.target = oOptions.target + "/" + oTarget.version;
-                        }
-                    }
-
-                }
-
-                if (oRoute.path && oTarget.entryPath) {
-                    var oRouteNew = {};
-                    var sPathOld = "^" + oRoute.path;
-                    oRouteNew[sPathOld] = oTarget.entryPath;
-                    oOptions.pathRewrite = oRouteNew;
-                }
-
-                app.use(oRoute.path, proxy(oOptions));
-
-            }
-
-        });
-    }
-
-    //redirect by default to launchpad
-    if (oNeoApp && oNeoApp.welcomeFile) {
-        app.get('/', function (req, res) {
-
-            var oURL = Object.assign({
-                pathname: oNeoApp.welcomeFile,
-                query: oSettings.queryParams || {}
-            });
-
-            if (oManifest && oManifest["sap.app"]) {
-                Object.assign(oURL, {
-                    query: {
-                        "sap-ushell-test-url-url": "../../../../../webapp",
-                        "sap-ushell-test-url-additionalInformation": "SAPUI5.Component=" + oManifest["sap.app"].id
-                    },
-                    hash: "Test-url"
-
-                });
-            }
-
-            res.redirect(url.format(oURL));
-        });
-    }
-
-    //static paths
-    ["appconfig", "webapp"].concat(oSettings.static).forEach(function (sPath) {
-        if (sPath) app.use("/" + sPath, express.static(sPath));
+  // redirect to FLP
+  app.get(homePage, async (req, res) => {
+    let flp = await fetch(cdn + homePage, {
+      agent: oAgent
     });
+    const $ = cheerio.load(await flp.text());
+    $("#sap-ui-bootstrap").attr().src = cdn + "/resources/sap-ui-core.js";
+    $("#sap-ushell-bootstrap").attr().src =
+      cdn + "/test-resources/sap/ushell/bootstrap/sandbox.js";
 
-    return app;
+    res.send($.html());
+  });
 
+  if (oNeoApp && oNeoApp.routes) {
+    oNeoApp.routes.forEach(function(oRoute) {
+      var oTarget = oRoute.target;
+      if (oTarget) {
+        // proxy options
+        var oOptions = {};
+
+        // search for destination
+        if (oDestinations && oTarget.name) {
+          var oDestination = oDestinations[oTarget.name];
+          if (oDestination) {
+            oOptions.target = oDestination.target;
+            oOptions.changeOrigin = true;
+            if (oDestination.useProxy) {
+              oOptions.agent = oAgent;
+            }
+
+            let sVersion = oSettings.version || oTarget.version;
+
+            if (oTarget.name === "sapui5" && sVersion) {
+              oOptions.target = oOptions.target + "/" + sVersion;
+            }
+          }
+        }
+
+        if (oRoute.path && oTarget.entryPath) {
+          var oRouteNew = {};
+          var sPathOld = "^" + oRoute.path;
+          oRouteNew[sPathOld] = oTarget.entryPath;
+          oOptions.pathRewrite = oRouteNew;
+        }
+
+        app.use(oRoute.path, proxy(oOptions));
+      }
+    });
+  }
+
+  //static paths
+  ["appconfig", "webapp"].concat(oSettings.static).forEach(function(sPath) {
+    if (sPath) app.use("/" + sPath, express.static(sPath));
+  });
+
+  return app;
 };
-
